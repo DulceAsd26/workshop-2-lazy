474c7baf91bfb412cc819afbc7dc6728ad53c479
DulceAsd26 <DulceAsd26@users.noreply.github.com>
2022-02-03 06:48:55 +0000

    deploy: bcbee9330238ed2f7d3958ebe5e2a04f44c3f813

---
+++ _dist_/index.js
@@ -0,0 +1,37 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+const minimum = 1; 
+const maximum = 123;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum
+
+const createImageNode = () => {
+    const container = document.createElement('div')
+    container.className = "p-4";
+
+    const imagen = document.createElement('img')
+    imagen.className = "mx-auto";
+    imagen.width = "320";
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const nuevaImagen = createImageNode()
+const mountNode = document.getElementById('images');
+
+const addButton = document.querySelector('button');
+
+//
+const addImage = () => {
+     const newImage = createImageNode();
+     mountNode.append(newImage);
+
+}
+
+
+addButton.addEventListener("click", addImage);
+//mountNode.append(nuevaImagen);
