db2b6c43f60d79fd39d5408c9c251812f4398748
DulceAsd26 <DulceAsd26@users.noreply.github.com>
2022-02-04 05:15:46 +0000

    deploy: 2ba465abe367cfb7e469a300050c265d6cd9fc91

--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -3,18 +3,22 @@ const isIntersecting = (entry) => {
     return entry.isIntersecting //true (dentro de la pantalla)
 };
 
-const accion = (entry) => {
-    const nodo = entry.target
-    console.log("Hey");
+const loadImage = (entry) => {
+    const container = entry.target; 
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
+    //carga la imagen
+    imagen.src = url;
+
 
     //desregistra la imagen
-    observer.unobserve(nodo)
+    observer.unobserve(container);
 };
 
 const observer = new IntersectionObserver((entries) => {
     entries
     .filter(isIntersecting)
-    .forEach(accion);
+    .forEach(loadImage);
 });
 export const registerImage = (imagen) => {
 
